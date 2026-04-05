b068417b726083c45a8e565c323e0474d7d39190
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-05 23:53:20 +0800

    加了多账号功能，多人签到

--- src/create_student.cr
+++ src/create_student.cr
@@ -3,6 +3,11 @@ require "json"
 require "log"
 require "./accounts.cr"
 
+# 剩余时间字段匹配的正则
+REMAINING_TIME_RE = /"remainingTime"\s*:\s*(\d+)/
+# 查看签到是否成功
+SUCCESS_RE = /"success"\s*:\s*(true|false)/
+
 class Student
   # 确定类型
   @name : String
@@ -28,18 +33,39 @@ class Student
     }
   end
 
+  # 签到post
   def post(courseSignInId : String, codeStringUrl : (String | Nil)) # 签到码的url有可能为nil, 所以为(String | Nil)
     # 在开始签到后，马上拼接url
     @post_url = "https://www.eduplus.net/api/course/clock_in/study?signInId=#{courseSignInId}#{codeStringUrl}"
     # 发出签到请求，并拿回响应
     response = HTTP::Client.post(@post_url, headers: @sign_in_headers)
 
-    if response.status_code == 200
-      # 签到成功
-      Log.info{"#{@name} 签到成功"}
+    # 探测签到状态
+    if match = SUCCESS_RE.match(response.body)
+      if match[1] == "true"
+        # 签到成功
+        Log.info{"用户 #{@name} 签到成功！！！"}
+      else
+        Log.info{"#{@name} 签到失败！！请手动签到，未获取到签到码"} if codeStringUrl.nil?
+        Log.info{"#{@name} 签到失败！！请手动签到，签到码#{codeStringUrl.split('=')[1]}"} if !codeStringUrl.nil?
+      end
     else
-      Log.info{"#{@name} 未签到成功！！请手动签到，未获取到签到码"} if codeStringUrl.nil?
-      Log.info{"#{@name} 未签到成功！！请手动签到，签到码#{codeStringUrl.split('=')[1]}"} if !codeStringUrl.nil?
+      Log.warn{"无法探测到 #{@name} 签到状态"}
+    end
+  end
+
+  # 获取剩余秒数，
+  def get_remaining_seconds(courseSignInId : String, codeStringUrl : String) : Int32 # 能获取秒数就肯定是密码签到，密码不能nil, 然后返回一个int32的秒数
+    response = HTTP::Client.get("https://www.eduplus.net/api/course/clock_in/#{courseSignInId}/student", @sign_in_headers)
+
+    # 匹配剩余时间字段
+    if match = REMAINING_TIME_RE.match(response.body)
+      # 返回剩余秒数
+      return match[1].to_i
+    else
+      Log.warn{"密码签到: #{codeStringUrl.split('=')[1]} 无法获取剩余时间,请手动签到"}
+      # 无法获取，就返回剩余时间为0
+      return 0
     end
   end
 end
--- src/sign_in.cr
+++ src/sign_in.cr
@@ -4,7 +4,8 @@ require "log"
 require "./create_student.cr"
 require "./accounts.cr"
 
-SIGN_IN_BELOW_HOW_MANY_SECONDS = 20
+# 到了这个剩余秒数，就要开始签到
+DEADLINE = 20
 
 class Signer
   def initialize()
@@ -21,17 +22,58 @@ class Signer
   def run(courseSignInId : String, codeDistance : String)
     # 判断为普通签到还是密码签到
     if codeDistance == "200"
-      # 普通签到，"200", 则签到url为空
+
+      # 普通签到，"200",
+      # 则签到url为空
       codeStringUrl = nil
+      # 直接进行签到post
+      sleep 1
+      @students.each do |student|
+        student.post(courseSignInId, codeStringUrl)
+      end
     else
-      # 如果为密码签到，拼接url和密码
+
+      # 密码签到
+      # codeDistance 不为200
+
+      # 拼接url和密码
       codeStringUrl = "&codeDistance=" + codeDistance
-    end
 
-    # 进行签到post
-    @students.each do |student|
-      student.post(courseSignInId, codeStringUrl)
-    end
+      # 默认第一个为默认学生
+      default_stu = @students[0]
 
+      # 开始循环get剩余时间
+      begin
+        loop do
+          # 获取剩余时间
+          remaining_seconds = default_stu.get_remaining_seconds(courseSignInId, codeStringUrl)
+          Log.info{"密码签到剩余 #{remaining_seconds} 秒"}
+
+          # 小于指定时间，开始签到
+          if remaining_seconds <= DEADLINE
+            @students.each do |student|
+              student.post(courseSignInId, codeStringUrl)
+            end
+            #签完到，退出
+            break
+          end
+
+          # 一秒拉取一回剩余时间
+          sleep 1
+        end
+
+      # 确保完成签到
+      ensure
+        Log.info{"二次签到开始"}
+        @students.each do |student|
+          student.post(courseSignInId, codeStringUrl)
+        end
+        Log.info{"二次签到完成"}
+      end
+
+    end
+    Log.info{"本次签到完毕"}
+    Log.info{"轮询重新开始..."}
+    Log.info{"------------------------------"}
   end
 end
