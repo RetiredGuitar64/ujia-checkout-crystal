323c03bac74b9f0d64615dc145441ec3299d93d4
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-24 20:06:53 +0800

    spec init

---
+++ spec/accounts_reader_spec.cr
@@ -0,0 +1,7 @@
+require "../src/accounts_reader.cr"
+require "spec"
+
+describe AccountsReader do
+  describe "#read_accounts" do
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/ujiacrystal"
+require "../src/"
